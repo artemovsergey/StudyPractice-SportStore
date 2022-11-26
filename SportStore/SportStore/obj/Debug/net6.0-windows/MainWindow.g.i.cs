8d1b8c7b46cb893d8255ddca5ef4f11ce7e7c19e
artik3314@gmail.com <artik3314@gmail.com>
2022-11-26 20:11:22 +0300

    Коммит из Visual Studio

--- SportStore/SportStore/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ SportStore/SportStore/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace SportStore {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SportStore;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SportStore;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
