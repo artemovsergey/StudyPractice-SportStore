dae411b0091d05d5de693ce6257537d84454f1e2
artik3314@gmail.com <artik3314@gmail.com>
2022-11-27 11:20:57 +0300

    Переход на главное окно

--- SportStore/SportStore/LoginWindow.xaml.cs
+++ SportStore/SportStore/LoginWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,12 @@ namespace SportStore
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Close();
         }
     }
 }
