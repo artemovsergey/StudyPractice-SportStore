9070486c04cfae9ed2b731f521542cbe4ffda1a7
artik3314@gmail.com <artik3314@gmail.com>
2022-12-01 22:32:12 +0300

    Рефакторинг работы с изображениями

--- SportStore/SportStore/AddProductWindow.xaml.cs
+++ SportStore/SportStore/AddProductWindow.xaml.cs
@@ -20,7 +20,12 @@ namespace SportStore
     public partial class AddProductWindow : Window
     {
 
+        Product? currentProduct;
+
         string? oldImage;
+        string? newImage;
+        string? newImagePath;
+
         public AddProductWindow()
         {
             InitializeComponent();
@@ -78,7 +83,6 @@ namespace SportStore
                 {
                     Product product = new Product()
                     {
-
                         ArticleNumber = articleBox.Text,
                         Name = nameBox.Text,
                         Description = descriptionBox.Text,
@@ -92,8 +96,6 @@ namespace SportStore
                         MaxDiscount = Convert.ToDecimal(maxDiscountBox.Text),
                         Supplier = supplierBox.Text,
                         Unit = unitBox.Text
-
-
                     };
 
 
@@ -112,9 +114,27 @@ namespace SportStore
                     db.Products.Add(product);
 
                     // если фото не выбрано
-                    if (String.IsNullOrEmpty(imageBox.Text))
+                    if (String.IsNullOrEmpty(newImage))
                     {
                         product.Photo = "picture.png";
+                        BitmapImage image = new BitmapImage(new Uri(product.ImagePath));
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        imageBoxPath.Source = image;
+                    }
+                    else
+                    {
+                        string newRelativePath = $"{System.DateTime.Now.ToString("HHmmss")}_{newImage}";
+                        product.Photo = newRelativePath;
+
+                        //MessageBox.Show($"newImage: {newImage}");
+                        //MessageBox.Show($"newImagePath: {newImagePath}");
+                        //MessageBox.Show(File.Exists(newImagePath).ToString());
+
+                       
+                        File.Copy(newImagePath, System.IO.Path.Combine(Environment.CurrentDirectory, $"images/{newRelativePath}"));
+
+                        
+
                         BitmapImage image = new BitmapImage(new Uri(product.ImagePath));
                         image.CacheOption = BitmapCacheOption.OnLoad;
                         imageBoxPath.Source = image;
@@ -122,13 +142,13 @@ namespace SportStore
              
 
                     db.SaveChanges();
-
+                    
                     MessageBox.Show("Продукт успешно добавлен!");
 
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.InnerException.ToString());
+                    MessageBox.Show(ex.Message.ToString());
                 }
 
 
@@ -141,6 +161,73 @@ namespace SportStore
         private void AddImageToProduct(object sender, RoutedEventArgs e)
         {
 
+            Stream myStream;
+
+            if (currentProduct != null)
+            {
+                oldImage = System.IO.Path.Combine(Environment.CurrentDirectory, $"images/{imageBox.Text}");
+            }
+            else
+            {
+                oldImage = null;
+            }
+
+            // проверяем, есть ли изображение у товара и запоминаем путь к изображению сейчас
+
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+            if (dlg.ShowDialog() == true)
+            {
+                if ((myStream = dlg.OpenFile()) != null)
+                {
+                    dlg.DefaultExt = ".png";
+                    dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+                    dlg.Title = "Open Image";
+                    dlg.InitialDirectory = "./";
+
+
+                    // Предпросмотр изображения
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.CreateOptions = BitmapCreateOptions.IgnoreImageCache;
+                    image.UriSource = new Uri(dlg.FileName);
+                    image.EndInit();
+                    
+
+                    //MessageBox.Show($"Изображения: {image.Width} на {image.Height} пикселей. Размер будет приведен к 200 на 300 пикселей! ");
+                    image.DecodePixelWidth = 200;
+                    image.DecodePixelHeight = 300;
+                    imageBoxPath.Source = image;
+
+                    try
+                    {
+                        
+                        newImage = dlg.SafeFileName;
+                        //MessageBox.Show($"newImage: {newImage}");
+                        newImagePath = dlg.FileName;
+                        //MessageBox.Show($"newImagePath: {newImagePath}");
+                        //MessageBox.Show(File.Exists(newImagePath).ToString());
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                myStream.Dispose();
+            }
+
+            MessageBox.Show($"Старое изображение: {oldImage} \n Новое изображение: {newImage} ");
+
         }
+            
+
+           
+
+           
+            
     }
 }
